39e14d01bbe430b500c29c3f2e264ce746594625
ZIPcde <votmoyyashik100@gmail.com>
2024-09-22 08:29:32 +0300

    login really token from mysql added

--- api/src/controllers/login.js
+++ api/src/controllers/login.js
@@ -1,24 +1,42 @@
 // api\src\controllers\login.js
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const secret = 'your_secret_key'; // Временно используем простой ключ
+const mysql = require('mysql2');
+
+// Временно используем простой ключ
+const secret = 'your_secret_key'; 
+
+// Настройка соединения с базой данных
+const db = mysql.createConnection({
+  host: process.env.MYSQL_ADMINS_HOST || 'mysql_admins',
+  user: process.env.MYSQL_ADMINS_USER,
+  password: process.env.MYSQL_ADMINS_PASSWORD,
+  database: process.env.MYSQL_ADMINS_DATABASE
+});
 
 // Пример функции авторизации
 function login(req, res) {
   const { username, password } = req.body;
 
   // Проверка пользователя в базе данных
-  const user = { id: 1, username, role: 'client', passwordHash: '$2a$12$MhHggqp0pCzZrzI9spnKsOrOAzJLaXjAWRmLsyHpMx/iF.SSU8bXS' }; // Замените на реальную проверку
-
-  // Сравниваем хеш пароля
-  bcrypt.compare(password, user.passwordHash, (err, result) => {
-    if (result) {
-      // Генерируем токен
-      const token = jwt.sign({ id: user.id, role: user.role }, secret, { expiresIn: '1h' });
-      res.json({ token });
-    } else {
-      res.sendStatus(403);
+  const query = 'SELECT * FROM admins WHERE username = ?'; // Предположим, что есть таблица admins
+  db.query(query, [username], (err, results) => {
+    if (err || results.length === 0) {
+      return res.sendStatus(403); // Пользователь не найден
     }
+
+    const user = results[0];
+
+    // Сравниваем хеш пароля
+    bcrypt.compare(password, user.passwordHash, (err, result) => {
+      if (result) {
+        // Генерируем токен
+        const token = jwt.sign({ id: user.id, role: user.role }, secret, { expiresIn: '1h' });
+        res.json({ token });
+      } else {
+        res.sendStatus(403);
+      }
+    });
   });
 }
 
